9b0d2fed9c4f972bcc7d84d3304f929cd152d032
Sukhvir Singh <namedsukhvir@gmail.com>
2025-09-22 13:53:54 -0300

    google anylatics

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import "aos/dist/aos.css";
 import { DefaultSeo } from "next-seo";
 import SEO from "../next-seo.config";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
@@ -17,6 +18,20 @@ export default function App({ Component, pageProps }) {
     <>
       {/* ✅ Global SEO config */}
       <DefaultSeo {...SEO} />
+
+      {/* ✅ Google Analytics (replace G-XXXXXXXXXX with your Measurement ID) */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-HP96DXQWLF');
+        `}
+      </Script>
 
       {/* Page content */}
       <Component {...pageProps} />
